48d653d8c7ad4ae23d5859a70abb69496719c1c3
Pierre-Yves Jezegou <salydu29@gmail.com>
2020-04-04 19:15:20 +0200

    Edited Config

--- weatherinsight/weatherinsight/settings.py
+++ weatherinsight/weatherinsight/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7nskbab6e&8$88t7(@a=!ez+1*21&dgpdu1+ny#q+d$cddz-i)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['weatherinsight.space', 'www.weatherinsight.space']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
