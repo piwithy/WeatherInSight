4400f61b1a60b80f16f482ed96cbf625bdddd9a5
Pierre-Yves Jezegou <salydu29@gmail.com>
2020-04-09 16:03:22 +0200

    Updated setup_env to check pip_req each time

--- Setup_environement.sh
+++ Setup_environement.sh
@@ -5,10 +5,10 @@ if [ ! -d "venv" ]; then
   echo "Setting up Virtual Environment"
   virtualenv venv --python=python3
   source "venv/bin/activate"
-  pip install -r "pip_req"
   deactivate
 fi
 source "venv/bin/activate"
+pip install -r "pip_req"
 python manage.py makemigrations
 python manage.py migrate
 python manage.py sqlmigrate pages 0001
