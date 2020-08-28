d3c681b7087b796c020ac41eef9dfaebe585af29
JEZEGOU Pierre-Yves <salydu29@gmail.com>
2020-08-28 19:20:38 +0200

    Cleaned-up project to start w/ Node

--- Prod_Start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-WORKING_DIR="$(dirname "$0")"
-cd "$WORKING_DIR" || exit 1
-bash -c "./Setup_environement.sh"
-source "venv/bin/activate"
-export "DJANGO_DEBUG=False"
-python manage.py collectstatic --no-input --clear
-python manage.py check --deploy
-gunicorn --access-logfile - --workers 3 --bind "unix:$WORKING_DIR/gunicorn.sock" WeatherInSight.wsgi:application
